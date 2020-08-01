7cff5841236bdc0802684a1dc96d5c200dead31d
Under0Cover <60072961+Under0Cover@users.noreply.github.com>
2020-08-01 17:35:47 -0300

    Add files via upload

---
+++ JavaScript/Aula 02/main.js
@@ -0,0 +1,45 @@
+var nome = 'Tales Oliver'
+var idade1 = 35
+var idade2 = 6
+var frase = 'Japão é o melhor time do Mundo!!'
+
+// alert('Meu primeiro JS!')
+// Criando o primeiro alert
+
+// alert(`Bem vindo, ${nome}`)
+// Chamada de string
+// Como eu já fiz outros cursos usei logo a crase
+
+// alert(`${nome} tem ${idade}`
+// Concatenando duas variavéis (string e números)
+
+// alert("O " +nome + " tem " +idade1 +idade2) */
+// Aqui o JS interpretou tudo como string
+
+// alert(idade1 +idade2) */
+// Aqui o JS interpretou tudo como número
+
+// console.log(nome)
+// console.log(idade + idade2)
+// Exemplificando a parte do 'Client Side' com o console.log usado no browser
+
+// console.log(frase)
+// Imprimindo a frase no console
+
+// console.log(frase.replace('Japão', 'Brasil'))
+// Aprendendo a fazer o replace no console
+
+// alert(frase.replace('Japão', 'Brasil'))
+// Fazendo replace no alert
+
+// console.log(frase.toUpperCase())
+// Todas maiúsculas
+
+// console.log(frase.toLowerCase())
+// Todas minúsculas
+
+console.log(idade1 + idade2)
+console.log(idade1 - idade2)
+console.log(idade1 * idade2)
+console.log(idade1 / idade2)
+// As 04 operações básicas da matemática
