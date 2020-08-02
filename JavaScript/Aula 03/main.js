e40cc3bf1f6c04970b4bdfe0aa83c392e4751f1a
Under0Cover <60072961+Under0Cover@users.noreply.github.com>
2020-08-02 14:16:02 -0300

    Add files via upload

---
+++ JavaScript/Aula 03/main.js
@@ -0,0 +1,26 @@
+// Apresentação de um If-Else básico, com e sem prompt
+/*
+var idade = prompt('Qual é a sua idade?')
+// var idade = 18
+if (idade >= 18){
+    alert('Maior de idade')
+} else {
+    alert('Menor de idade')
+} */
+
+
+// Criando laços de repetição
+var count = 0
+while (count <= 5){
+    console.log(count)
+    count++ 
+}
+
+var contar 
+for (contar = 0; contar <= 5; contar ++) {
+    console.log(contar)
+}
+
+var data = new Date()
+alert(d)
+// Imprimindo a data do sistema
