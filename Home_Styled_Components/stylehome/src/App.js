2afff582ceb39089f61d079c30dcf5c591a340a1
Tales Oliver <talis.oliveira@hotmail.com.br>
2021-05-01 11:33:16 -0300

    Without saving Hello, React World!

--- Home_Styled_Components/stylehome/src/App.js
+++ Home_Styled_Components/stylehome/src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          Hello, React Word!
+          Hello, React World!
         </p>
         <a
           className="App-link"
